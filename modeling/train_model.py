3eeaebbe321355915868b5d8636bc42688fe04f6
cae0027 <celeh@aust.edu.ng>
2023-03-15 11:56:00 -0500

    initial test function

--- modeling/train_model.py
+++ modeling/train_model.py
@@ -1,15 +1,28 @@
+
+import sys
+sys.path.append('../sources')
+
 # Local import
 from architecture import CVAE
 from train_helpers import final_loss, fit, validate, device
 from load_data import load_data
+from fem_error_1d import EvalCoarseSoln
 # Built in import
+import torch
+import torch.functional as F
 import torch.optim as optim
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 
 in_features, out_features = 22, 2001
-def run_model(in_features=22, out_features=2001, epochs=200, lr=0.0005, batch_size=30):
+batch_size = 30
+# load data
+fine_path = '../data-gen/fine_scale_data_y.npy'
+coarse_path = '../data-gen/coarse_scale_data_y.npy'
+train_loaderf, val_loaderf, train_loaderc, val_loaderc = load_data(coarse_path, fine_path, split_frac=0.8, batch_size=batch_size)
+
+def run_train(in_features=22, out_features=2001, epochs=200, lr=0.0005, batch_size=batch_size):
     """
     Great for easy calls during hyperparameter tuning
     """
@@ -19,12 +32,6 @@ def run_model(in_features=22, out_features=2001, epochs=200, lr=0.0005, batch_si
     batch_size = batch_size
     optimizer = optim.Adam(model.parameters(), lr=lr)
     criterion = nn.MSELoss()
-
-
-    # load data
-    fine_path = '../data-gen/fine_scale_data_y.npy'
-    coarse_path = '../data-gen/coarse_scale_data_y.npy'
-    train_loaderf, val_loaderf, train_loaderc, val_loaderc = load_data(coarse_path, fine_path, split_frac=0.8, batch_size=batch_size)
 
     # load x for plotting
     x = np.load('../data-gen/fine_scale_data_x.npy').astype(np.float32)
@@ -59,6 +66,27 @@ def run_model(in_features=22, out_features=2001, epochs=200, lr=0.0005, batch_si
     plt.plot(x, model_result.T)
     plt.show()
     return x, model_result, soln_true, model
+criterion = nn.MSELoss()
+# comp = EvalCoarseSoln()
+error = []
+def run_test(model):
+    for ycb, yfb in zip(val_loaderc, val_loaderf):
+        for yc, yf in zip(ycb, yfb):
+            z = torch.randn((1,in_features))
+            z = torch.cat((z, yc), dim=1)
+
+            y = F.relu(model.dec1(z))
+            y = F.relu(model.dec2(y))
+            y = F.relu(model.dec3(y))
+            y = model.dec4(y)
+            y = y[0, :].detach().numpy()
+            err = comp.error(y, yf)
+            error.append(err)
+            # plt.plot(x, y[0, :].detach().numpy())
+            # plt.plot(x, yf)
+    plt.plot(error)
+    return np.array(error)
+
 
 if __name__ == '__main__':
-    run_model()
+    run_train()
