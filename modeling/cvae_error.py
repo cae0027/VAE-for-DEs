76590e3308d81275bdc17fec255264c4416cef68
Chinedu Eleh <celeh@aust.edu.ng>
2023-03-15 09:11:14 -0500

    CVAE error function

---
+++ modeling/cvae_error.py
@@ -0,0 +1,28 @@
+"""
+Compute error between CVAE solution and true finite element solution using inner product induced by the stiffness matrix corresponding to the fine scale solutions
+"""
+
+import sys
+sys.path.append('../sources')
+
+from train_model import run_model
+from fem_error_1d import EvalCoarseSoln
+import matplotlib.pyplot as plt
+
+
+# run CVAE to get reconstructed solutions, along with true solns
+x, model_result, true_soln = run_model(epochs=500)
+
+# boundary points
+a = -1  
+b = 1
+# instantiate error class
+comp = EvalCoarseSoln(a=a, b=b)
+
+# compute error btw reconstucted solns and true solns
+error = []
+for i in range(len(model_result)):
+    err = comp.error(model_result[i], true_soln[i])
+    error.append(err)
+plt.plot(error)
+plt.show()
