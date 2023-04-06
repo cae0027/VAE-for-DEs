b4dc9b073e9074b531368e326b1a9005c1854a34
cae0027 <celeh@aust.edu.ng>
2023-04-06 12:14:47 -0500

    calculate run time in hours instead

--- modeling/params_optimization.py
+++ modeling/params_optimization.py
@@ -12,7 +12,6 @@ import sys
 sys.path.append('../sources')
 
 from train_model import run_train, run_test
-from fem_error_1d import EvalCoarseSoln
 from dynamic_layers import net_inp_out_sizes
 import numpy as np
 import pickle
@@ -25,26 +24,20 @@ import colorama
 colorama.init(autoreset=True)
 ##################################################################
 
-# # boundary points
-# a = -1 
-# b = 1
-# # instantiate error class
-# comp = EvalCoarseSoln(a=a, b=b)
-
 search_result = {'num_layers':[], 'learning_rate':[], 'epoch':[], 'batch_size':[], 'latent_dim':[], 'layer_inputs':[], 'relativeRMSE':[], 'relativeL2error':[]}
 # Parameters to optimize
 num_layers = np.arange(1, 11)
 now = time.time()
 for i, layer in enumerate(num_layers):
     # run multiple times to explore different network input-output sizes or a given number of layers
-    for _ in range(1000):
+    for _ in range(10000):
         params = net_inp_out_sizes(no_layers=layer)
         latent_dim = params[1][-1]
         batch = np.random.randint(5, 200)
         lr = round(np.random.uniform(1e-06, 0.1), 7)
         epoch = np.random.randint(2, 500)
         # run model at least two times to account for randomness in model weights
-        for _ in range(3):
+        for _ in range(4):
             _, _, _, model = run_train(epochs=epoch, lr=lr, batch_size=batch, layer_params=params)
             accRMSE, accL2 = run_test(model, optim_params=True)
             params_collect = [layer, lr, epoch, batch, latent_dim, model.layer_inputs, accRMSE, accL2]
